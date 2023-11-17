083e2e85c60ff51dc38bb937340a1b76497622aa
krstevskipetar <krstevski1petar@gmail.com>
2023-11-17 17:47:27 +0100

    Construct basic train loop.

--- train.py
+++ train.py
@@ -1,14 +1,23 @@
+import math
+import sys
+
+from tqdm import tqdm
+
+from utils import collate_fn, reduce_dict
 from yolo_dataset import YOLODataset
 import torch
 import argparse
 from model import get_model
+import numpy as np
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_path', default='G:\\waste_dataset_v2\\train\\images')
-    parser.add_argument('--annotation_path', default='G:\\waste_dataset_v2\\train\\labels')
-    parser.add_argument('--label_file', default='G:\\waste_dataset_v2\\label_map.txt')
+    parser.add_argument('--image_path', default='/home/petar/waste_dataset_v2/train/images')
+    parser.add_argument('--annotation_path', default='/home/petar/waste_dataset_v2/train/labels')
+    parser.add_argument('--image_path_val', default='/home/petar/waste_dataset_v2/val/images')
+    parser.add_argument('--annotation_path_val', default='/home/petar/waste_dataset_v2/val/labels')
+    parser.add_argument('--label_file', default='/home/petar/waste_dataset_v2/label_map.txt')
     parser.add_argument('--shuffle', action='store_true', default=False)
     argz = parser.parse_args()
 
@@ -16,8 +25,12 @@ def parse_args():
 
 
 args = parse_args()
-
 dataset = YOLODataset(image_path=args.image_path,
+                      annotation_path=args.annotation_path,
+                      label_file=args.label_file,
+                      shuffle=args.shuffle)
+
+dataset_val = YOLODataset(image_path=args.image_path,
                       annotation_path=args.annotation_path,
                       label_file=args.label_file,
                       shuffle=args.shuffle)
@@ -25,20 +38,79 @@ dataset = YOLODataset(image_path=args.image_path,
 data_loader = torch.utils.data.DataLoader(
     dataset,
     batch_size=2,
-    shuffle=True,
-    num_workers=4,
-    collate_fn=torch.utils.data.collate_fn
+    shuffle=False,
+    num_workers=1,
+    collate_fn=collate_fn,
+    drop_last=True
+)
+data_loader_val = torch.utils.data.DataLoader(
+    dataset_val,
+    batch_size=2,
+    shuffle=False,
+    num_workers=1,
+    collate_fn=collate_fn,
+    drop_last=True
 )
 
 model = get_model()
 
-images, targets = next(iter(data_loader))
-images = list(image for image in images)
-targets = [{k: v for k, v in t.items()} for t in targets]
-output = model(images, targets)  # Returns losses and detections
-print(output)
 
 
+
+# construct an optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(
+    params,
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005
+)
+
+# and a learning rate scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer,
+    step_size=3,
+    gamma=0.1
+)
+
+# let's train it for 5 epochs
+num_epochs = 5
+
+for epoch in range(num_epochs):
+    # train for one epoch, printing every 10 iterations
+    model.train()
+    loss_value=0
+    for idx, (images, targets) in tqdm(enumerate(data_loader), total=len(data_loader)):
+        images = list(image for image in images)
+        output = model(images, targets)  # Returns losses and detections
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        loss_value = losses_reduced.item()
+        if not math.isfinite(loss_value):
+            print(f"Loss is {loss_value}, stopping training")
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+        # print(loss_value)
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss_value,
+    }, 'checkpoints')
+
+
+print("That's it!")
 def main():
     pass
 
--- yolo_dataset.py
+++ yolo_dataset.py
@@ -1,3 +1,4 @@
+import pdb
 import random
 
 import numpy as np
@@ -5,7 +6,9 @@ import torchvision.transforms
 import os
 
 import torch.utils.data
+from torchvision import tv_tensors
 from torchvision.io import read_image
+from torchvision.transforms.v2 import functional as F
 
 
 def yolobbox2bbox(x, y, w, h):
@@ -14,11 +17,12 @@ def yolobbox2bbox(x, y, w, h):
     return x1, y1, x2, y2
 
 
-resize = torchvision.transforms.Resize(640)
+resize = torchvision.transforms.Resize(640, antialias=True)
 
 
 class YOLODataset(torch.utils.data.Dataset):
 
+    # todo: add input validation
     def __init__(self, image_path, annotation_path, label_file, transform=None, target_transforms=None, shuffle=False):
 
         self.image_path = image_path
@@ -40,23 +44,47 @@ class YOLODataset(torch.utils.data.Dataset):
 
         self.class_dict = {class_name: i for i, class_name in enumerate(self.class_names)}
 
+    def _get_annotation(self, image_id):
+        with open(os.path.join(self.annotation_path, self.annotation_files[image_id]), 'r') as f:
+            ann_file = f.readlines()
+            ann_file = [[float(a) for a in ann_line.strip().split(' ')] for ann_line in ann_file]
+
+        ann_file = np.array(ann_file)
+        labels = ann_file[:, 0].astype(int)
+        boxes = ann_file[:, 1:]
+        return boxes.tolist(), labels.tolist()
+
     def __getitem__(self, index):
         image_id = self.ids[index]
         boxes, labels = self._get_annotation(image_id)
         image = read_image(os.path.join(self.image_path, self.image_files[image_id]))
+
         if image.shape[0] > 3:
             image = image[:3, :, :]
         image = image.float()
         size = image.size()[1:3]
         image = resize(image)
+        image = tv_tensors.Image(image)
 
-        x_scale = size[0] / image.size()[1]
-        y_scale = size[1] / image.size()[2]
-
+        x_scale = size[0] / image.size()[1] * 640
+        y_scale = size[1] / image.size()[2] * 640
         # Update bounding box coordinates
         boxes = [bb * torch.tensor([x_scale, y_scale, x_scale, y_scale]) for bb in torch.tensor(boxes)]
-        print(torch.stack(boxes))
-        return image, torch.stack(boxes), labels
+        boxes_ = []
+        for box in boxes:
+            boxes_.append([box[0], box[1], box[0] + box[2], box[1] + box[3]])
+        boxes = np.array(boxes_)
+
+        labels = torch.tensor([int(l) for l in labels])
+
+        iscrowd = torch.zeros((len(labels),), dtype=torch.int64)
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        target = {"boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY",
+                                                    canvas_size=F.get_size(image)),
+                  "labels": labels,
+                  "area": area,
+                  "iscrowd": iscrowd}
+        return image, target
 
     def __len__(self):
         return len(self.ids)
