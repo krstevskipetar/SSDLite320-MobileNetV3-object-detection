Fix bbox reformating and scaling

train.py
@@ -30,10 +30,10 @@ dataset = YOLODataset(image_path=args.image_path,
                       label_file=args.label_file,
                       shuffle=args.shuffle)
 
-dataset_val = YOLODataset(image_path=args.image_path,
-                      annotation_path=args.annotation_path,
-                      label_file=args.label_file,
-                      shuffle=args.shuffle)
+dataset_val = YOLODataset(image_path=args.image_path_val,
+                          annotation_path=args.annotation_path_val,
+                          label_file=args.label_file,
+                          shuffle=args.shuffle)
 
 data_loader = torch.utils.data.DataLoader(
     dataset,
@@ -54,9 +54,6 @@ data_loader_val = torch.utils.data.DataLoader(
 
 model = get_model()
 
-
-
-
 # construct an optimizer
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(
@@ -75,11 +72,14 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(
 
 # let's train it for 5 epochs
 num_epochs = 5
-
+def train_epoch():
+    pass
+def validate(model, val_loader):
+    pass
 for epoch in range(num_epochs):
     # train for one epoch, printing every 10 iterations
     model.train()
-    loss_value=0
+    loss_value = 0
     for idx, (images, targets) in tqdm(enumerate(data_loader), total=len(data_loader)):
         images = list(image for image in images)
         output = model(images, targets)  # Returns losses and detections
@@ -109,8 +109,9 @@ for epoch in range(num_epochs):
         'loss': loss_value,
     }, 'checkpoints')
 
-
 print("That's it!")
+
+
 def main():
     pass
 

yolo_dataset.py
@@ -1,9 +1,11 @@
 import pdb
 import random
+from typing import Sequence
 
 import numpy as np
 import torchvision.transforms
 import os
+from os.path import exists, join
 
 import torch.utils.data
 from torchvision import tv_tensors
@@ -11,13 +13,67 @@ from torchvision.io import read_image
 from torchvision.transforms.v2 import functional as F
 
 
-def yolobbox2bbox(x, y, w, h):
+def yolobbox2bbox(bbox: Sequence[int]):
+    x, y, w, h = bbox
     x1, y1 = x - w / 2, y - h / 2
     x2, y2 = x + w / 2, y + h / 2
-    return x1, y1, x2, y2
+    return int(x1), int(y1), int(x2), int(y2)
 
 
-resize = torchvision.transforms.Resize(640, antialias=True)
+def scale_bbox(bbox: Sequence[int], image_width: int, image_height: int):
+    x, y, w, h = bbox
+    x *= image_width
+    w *= image_width
+    y *= image_height
+    h *= image_height
+    return x, y, w, h
+
+
+resize = torchvision.transforms.Resize((640, 640), antialias=True)
+
+
+def normalized_to_absolute(bbox, image_width, image_height):
+    """
+    Convert normalized bounding box coordinates to absolute coordinates.
+
+    Parameters:
+    - bbox: Tuple (x, y, w, h) representing normalized coordinates.
+    - image_width: Width of the image.
+    - image_height: Height of the image.
+
+    Returns:
+    - Tuple (x_min, y_min, x_max, y_max) representing absolute coordinates.
+    """
+    x, y, w, h = bbox
+    x_min = int(x * image_width)
+    y_min = int(y * image_height)
+    x_max = int((x + w) * image_width)
+    y_max = int((y + h) * image_height)
+    return x_min, y_min, x_max, y_max
+
+
+def resize_bbox(bbox, original_size, new_size):
+    """
+    Resize bounding box coordinates to a new image size.
+
+    Parameters:
+    - bbox: Tuple (x_min, y_min, x_max, y_max) representing absolute coordinates.
+    - original_size: Tuple (original_width, original_height) representing the original image size.
+    - new_size: Tuple (new_width, new_height) representing the new image size.
+
+    Returns:
+    - Tuple (resized_x_min, resized_y_min, resized_x_max, resized_y_max) representing resized coordinates.
+    """
+    x_min, y_min, x_max, y_max = bbox
+    original_width, original_height = original_size
+    new_width, new_height = new_size
+
+    resized_x_min = int(x_min * (new_width / original_width))
+    resized_y_min = int(y_min * (new_height / original_height))
+    resized_x_max = int(x_max * (new_width / original_width))
+    resized_y_max = int(y_max * (new_height / original_height))
+
+    return resized_x_min, resized_y_min, resized_x_max, resized_y_max
 
 
 class YOLODataset(torch.utils.data.Dataset):
@@ -36,13 +92,33 @@ class YOLODataset(torch.utils.data.Dataset):
         self.ids = [i for i in range(len(os.listdir(image_path)))]
 
         self.annotation_files = [''.join(img.split('.')[:-1]) + '.txt' for img in self.image_files]
-
+        self._validate_input()
         with open(label_file, 'r') as f:
             classes = f.readlines()
             classes = [c.strip() for c in classes]
             self.class_names = classes
 
         self.class_dict = {class_name: i for i, class_name in enumerate(self.class_names)}
+
+    def _validate_input(self):
+        i = 0
+        for idx, (image_file, annotation_file) in enumerate(zip(self.image_files, self.annotation_files)):
+            if not exists(join(self.image_path, image_file)) or not exists(join(self.image_path, image_file)):
+                self.image_files.pop(idx)
+                self.annotation_files.pop(idx)
+                i += 1
+            try:
+                boxes, labels = self._get_annotation(idx)
+                if np.shape(boxes)[1] > 4:
+                    self.annotation_files.pop(idx)
+                    self.image_files.pop(idx)
+                    i += 1
+            except:
+                self.image_files.pop(idx)
+                self.annotation_files.pop(idx)
+                i += 1
+
+        print(f"Removed {i} invalid samples")
 
     def _get_annotation(self, image_id):
         with open(os.path.join(self.annotation_path, self.annotation_files[image_id]), 'r') as f:
@@ -61,22 +137,17 @@ class YOLODataset(torch.utils.data.Dataset):
 
         if image.shape[0] > 3:
             image = image[:3, :, :]
-        image = image.float()
-        size = image.size()[1:3]
+        height, width = image.size()[1:3]
         image = resize(image)
-        image = tv_tensors.Image(image)
 
-        x_scale = size[0] / image.size()[1] * 640
-        y_scale = size[1] / image.size()[2] * 640
-        # Update bounding box coordinates
-        boxes = [bb * torch.tensor([x_scale, y_scale, x_scale, y_scale]) for bb in torch.tensor(boxes)]
-        boxes_ = []
+        _boxes = []
         for box in boxes:
-            boxes_.append([box[0], box[1], box[0] + box[2], box[1] + box[3]])
-        boxes = np.array(boxes_)
+            x, y, w, h = scale_bbox(box, width, height)
+            x1, y1, x2, y2 = yolobbox2bbox(bbox=[x, y, w, h])
+            _boxes.append([x1, y1, x2, y2])
+        boxes = np.array(_boxes)
 
         labels = torch.tensor([int(l) for l in labels])
-
         iscrowd = torch.zeros((len(labels),), dtype=torch.int64)
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         target = {"boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY",