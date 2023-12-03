Fix mean average precision calculations. Fix background class id bug.

engine.py
@@ -10,12 +10,14 @@ from metrics import calculate_ap_ar_map
 import time
 
 
-def validate(model, data_loader, device, iou_thresholds=None):  # todo: implement custom validation function
+def validate(model, data_loader, class_names, device, iou_thresholds=None):
     model.eval()
     model = model.to(device)
     if iou_thresholds is None:
         iou_thresholds = np.arange(0.1, 1, 0.1)
     all_ap, all_ar, all_map = [], [], []
+    all_class_precisions, all_class_recalls = ({class_name: [] for class_name in class_names},
+                                               {class_name: [] for class_name in class_names})
     start_time = time.time()
     for idx, (images, targets) in tqdm(enumerate(data_loader), total=len(data_loader), position=0, leave=True):
         images = [image.to(device) for image in images]
@@ -23,18 +25,23 @@ def validate(model, data_loader, device, iou_thresholds=None):  # todo: implemen
 
         targets = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in targets]
 
-        ap, ar, mean_ap = calculate_ap_ar_map(output, targets, iou_thresholds)
-        all_ap.append(ap)
-        all_ar.append(ar)
-        all_map.append(mean_ap)
+        mean_ap, mean_ar, class_precisions, class_recalls = calculate_ap_ar_map(output, targets, class_names,
+                                                                                iou_thresholds)
 
+        for key in class_names:
+            all_class_precisions[key].append(class_precisions[key])
+            all_class_recalls[key].append(class_recalls[key])
+
+        all_ap.append(mean_ap)
+        all_ar.append(mean_ar)
     end_time = time.time()
     print("Inference took ", end_time - start_time, " seconds")
-    ap, ar, mean_ap = np.mean(all_ap), np.mean(all_ar), np.mean(all_map)
-    print("Average precision: ", ap)
-    print("Average recall: ", ar)
-    print(f"mAP for IoU thresholds {iou_thresholds}: ", mean_ap)
-    return ap, ar, mean_ap
+    mean_ap, mean_ar = np.mean(all_ap), np.mean(all_ar)
+    for key in class_names:
+        all_class_precisions[key] = np.mean(all_class_precisions[key])
+        all_class_recalls[key] = np.mean(all_class_recalls[key])
+
+    return mean_ap, mean_ar, all_class_precisions, all_class_recalls
 
 
 def train_epoch(model, optimizer, lr_scheduler, data_loader, print_freq=100, epoch_number=0, all_losses=None):

metrics.py
@@ -36,8 +36,8 @@ def calculate_precision_recall(predictions, targets, iou_thresholds=None):
                                               targets['boxes'][matched_gt_index + 1:]), dim=0)
             else:
                 false_positives += 1
-    precision = true_positives / (true_positives + false_positives)
-    recall = true_positives / total_gt_boxes
+    precision = true_positives / (true_positives + false_positives + 1e-12)
+    recall = true_positives / (total_gt_boxes + 1e-12)
 
     return precision, recall
 
@@ -57,63 +57,58 @@ def calculate_iou(bbox1, bbox2):
     return iou
 
 
-def calculate_ap_ar_map(predictions, ground_truth, iou_thresholds=None):
+def filter_values_by_class(values: list, class_id: int):
+    filtered_values = []
+    for val in values:
+        index = val['labels'] == class_id
+        if not index.any():
+            continue
+        filtered_val = {}
+        for key in val.keys():
+            if key == 'image_id':
+                filtered_val.update({key: val[key]})
+            else:
+                filtered_val.update({key: val[key][index]})
+        filtered_values.append(filtered_val)
+    return filtered_values
+
+
+def calculate_ap_ar_map(predictions, ground_truth, class_names, iou_thresholds=None):
     if iou_thresholds is None:
         iou_thresholds = [0.5]
-
+    kept_predictions = []
+    for pred in predictions:
+        keep = apply_nms(pred['boxes'], pred['scores'], threshold=0.2, score_threshold=0.05)
+        kept_predictions.append({'boxes': pred['boxes'][keep],
+                                 'labels': pred['labels'][keep],
+                                 'scores': pred['scores'][keep]})
+    predictions = kept_predictions
     total_precision = 0
     total_recall = 0
-    total_map = 0
+    num_classes = len(class_names)
+    class_name_dict = {i: class_name for (i, class_name) in enumerate(class_names)}
+    class_precisions, class_recalls = {}, {}
+    for class_id in range(num_classes):
+        class_predictions = filter_values_by_class(values=predictions, class_id=class_id)
+        class_ground_truth = filter_values_by_class(values=ground_truth, class_id=class_id)
+        total_precision_class = 0
+        total_recall_class = 0
+        for preds, gt in zip(class_predictions, class_ground_truth):
+            precision, recall = calculate_precision_recall(preds, gt, iou_thresholds)
+            total_precision_class += precision
+            total_recall_class += recall
 
-    for preds, gt in zip(predictions, ground_truth):
-        precision, recall = calculate_precision_recall(preds, gt, iou_thresholds)
-        total_precision += precision
-        total_recall += recall
-        total_map += calculate_average_precision(preds, gt, iou_thresholds)
+        num_samples_class = len(class_predictions)
+        ap_class = total_precision_class / num_samples_class if num_samples_class > 0 else 0
+        ar_class = total_recall_class / num_samples_class if num_samples_class > 0 else 0
 
-    num_samples = len(predictions)
-    ap = total_precision / num_samples if num_samples > 0 else 0
-    ar = total_recall / num_samples if num_samples > 0 else 0
-    mean_ap = total_map / num_samples if num_samples > 0 else 0
+        class_precisions.update({class_name_dict[class_id]: ap_class})
+        class_recalls.update({class_name_dict[class_id]: ar_class})
 
-    return ap, ar, mean_ap
+        total_precision += ap_class
+        total_recall += ar_class
 
+    mean_ap = total_precision / num_classes if num_classes > 0 else 0
+    mean_ar = total_recall / num_classes if num_classes > 0 else 0
 
-def calculate_average_precision(predictions, targets, iou_thresholds=None):
-    if iou_thresholds is None:
-        iou_thresholds = [0.5]
-
-    true_positives = 0
-    false_positives = 0
-    total_gt_boxes = len(targets['boxes'])
-    precision_values = []
-
-    if total_gt_boxes == 0:
-        return 0
-
-    for iou_threshold in iou_thresholds:
-        keep = apply_nms(predictions['boxes'], predictions['scores'], threshold=0.2, score_threshold=0.05)
-        kept_predictions = {'boxes': predictions['boxes'][keep],
-                            'labels': predictions['labels'][keep]}
-        for i, (pred_boxes, pred_labels) in enumerate(zip(kept_predictions['boxes'], kept_predictions['labels'])):
-            max_iou = 0
-            matched_gt_index = -1
-
-            for j, (gt_box, gt_label) in enumerate(zip(targets['boxes'], targets['labels'])):
-                iou = calculate_iou(pred_boxes, gt_box)
-                if iou > max_iou and gt_label == pred_labels:
-                    max_iou = iou
-                    matched_gt_index = j
-
-            if max_iou >= iou_threshold:
-                true_positives += 1
-                targets['boxes'] = torch.cat((targets['boxes'][:matched_gt_index],
-                                              targets['boxes'][matched_gt_index + 1:]), dim=0)
-            else:
-                false_positives += 1
-
-            precision = true_positives / (true_positives + false_positives)
-            precision_values.append(precision)
-
-    average_precision = sum(precision_values) / len(precision_values) if precision_values else 0
-    return average_precision
+    return mean_ap, mean_ar, class_precisions, class_recalls

train.py
@@ -36,7 +36,7 @@ def parse_args():
     parser.add_argument('--num_epochs', type=int, default=100)
     parser.add_argument('--print_freq', type=int, default=100)
     parser.add_argument('--evaluate_every', type=int, default=5)
-    parser.add_argument('--num_classes', type=int, default=4)
+    parser.add_argument('--num_classes', type=int, default=5)
     parser.add_argument('--trainable_backbone_layers', type=int, default=6)
     parser.add_argument('--weights', default=None)
     parser.add_argument('--wandb_logging', default=True)
@@ -68,10 +68,22 @@ def run_training(data_loader_train, data_loader_val, model, optimizer, lr_schedu
         }, f'checkpoints/epoch_{epoch}.pth')
         if epoch != 0 and epoch % evaluate_every == 0:
             print("Evaluation on val set")
-            ap, ar, mean_ap = validate(model, data_loader_val, torch.device(device), [0.5])
-            metrics['average_precision'].append(ap)
-            metrics['average_recall'].append(ar)
+            mean_ap, mean_ar, class_precisions, class_recalls = validate(model, data_loader_val, class_names,
+                                                                         torch.device(device), [0.5])
             metrics['mAP@50'].append(mean_ap)
+            metrics['mAR@50'].append(mean_ar)
+
+            print("For iou thresholds", 0.5)
+            print(f"Mean Average Precision: {mean_ap}\nMean Average Recall: {mean_ar}")
+
+            print("Class precisions: ")
+            for key, value in class_precisions.items():
+                print(f"\t-{key}: {value}")
+
+            print("Class recalls: ")
+            for key, value in class_recalls.items():
+                print(f"\t-{key}: {value}")
+
         last_checkpoint = f'checkpoints/epoch_{epoch}.pth'
 
     figures = infer_and_plot_batch_predictions(model, data_loader_val, class_names, 5)

validate.py
@@ -34,6 +34,7 @@ def parse_args():
     parser.add_argument('--shuffle', action='store_true', default=False)
     parser.add_argument('--device', default='cpu')
     parser.add_argument('--checkpoint', default="checkpoints/checkpoints/epoch_99.pth")
+    parser.add_argument('--num_classes', type=int, default=4)
     parser.add_argument('--wandb_logging', action='store_true', default=False)
     parser.add_argument('--wandb_project_name')
     argz = parser.parse_args()
@@ -75,15 +76,26 @@ def main():
         drop_last=True
     )
     device = torch.device(args.device)
-    model = get_model(num_classes=4, trainable_backbone_layers=6, weights=None)
+    model = get_model(num_classes=args.num_classes)
     if args.checkpoint:
         model.load_state_dict(torch.load(args.checkpoint, map_location=device)['model_state_dict'])
     model.eval()
+    iou_thresholds = [0.5]
+    mean_ap, mean_ar, class_precisions, class_recalls = validate(model=model,
+                                                                 data_loader=data_loader_val,
+                                                                 class_names=class_names,
+                                                                 device=device,
+                                                                 iou_thresholds=iou_thresholds)
+    print("For iou thresholds", iou_thresholds)
+    print(f"Mean Average Precision: {mean_ap}\nMean Average Recall: {mean_ar}")
 
-    ap, ar, mean_ap = validate(model=model,
-                               data_loader=data_loader_val,
-                               device=device,
-                               iou_thresholds=[0.5])
+    print("Class precisions: ")
+    for key, value in class_precisions.items():
+        print(f"\t-{key}: {value}")
+
+    print("Class recalls: ")
+    for key, value in class_recalls.items():
+        print(f"\t-{key}: {value}")
 
     config = {
         "run_type": "validation",
@@ -100,9 +112,12 @@ def main():
         "epochs": 100,
         "checkpoint": args.checkpoint
     }
-    metrics = {'average_precision': ap,
-               'average_recall': ar,
-               'mAP@50': mean_ap}
+    metrics = {'mAP@50': mean_ap,
+               'mAR@50': mean_ar}
+    metrics.update(
+        {str(class_name) + '_precision': class_precision for class_name, class_precision in class_precisions.items()})
+    metrics.update(
+        {str(class_name) + '_recall': class_recall for class_name, class_recall in class_recalls.items()})
     figures = infer_and_plot_batch_predictions(model, data_loader_val, class_names, 5)
     log_to_wandb(args.wandb_project_name, config, metrics, figures, args.checkpoint)
 

yolo_dataset.py
@@ -42,7 +42,8 @@ class YOLODataset(torch.utils.data.Dataset):
             classes = [c.strip() for c in classes]
             self.class_names = classes
 
-        self.class_dict = {class_name: i for i, class_name in enumerate(self.class_names)}
+        self.class_dict = {class_name: i + 1 for i, class_name in
+                           enumerate(self.class_names)}  # +1 for background class
 
     @staticmethod
     def yolobbox2bbox(bbox: Sequence[int]):
@@ -95,7 +96,7 @@ class YOLODataset(torch.utils.data.Dataset):
             ann_file = [[float(a) for a in ann_line.strip().split(' ')] for ann_line in ann_file]
 
         ann_file = np.array(ann_file)
-        labels = ann_file[:, 0].astype(int)
+        labels = ann_file[:, 0].astype(int) + 1  # +1 for background class
         boxes = ann_file[:, 1:]
         return boxes.tolist(), labels.tolist()
 