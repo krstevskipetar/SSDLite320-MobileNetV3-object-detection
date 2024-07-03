Fix bug where last client's weights were not taken into account for averaging. Save loss on client-side.

federated_training/client_finetuning.py
@@ -104,10 +104,10 @@ class ClientFineTune:
                                               map_location=self.device)['model_state_dict'])
 
         print("Checkpoint loaded, training one epoch...")
-        train_epoch(model=self.model,
-                    optimizer=self.optimizer,
-                    data_loader=self.data_loader,
-                    device=self.device)
+        all_losses, mean_loss = train_epoch(model=self.model,
+                                            optimizer=self.optimizer,
+                                            data_loader=self.data_loader,
+                                            device=self.device)
 
         torch.save({
             'model_state_dict': self.model.state_dict(),
@@ -119,4 +119,4 @@ class ClientFineTune:
         send_file(self.server_address, self.server_port, self.checkpoint)
         end_transfer_2 = perf_counter()
         transfer_time_2 = end_transfer_2 - start_transfer_2
-        return transfer_time, transfer_time_2, inference_time, training_time
+        return transfer_time, transfer_time_2, inference_time, training_time, all_losses, mean_loss

federated_training/federated_averaging.py
@@ -87,7 +87,8 @@ class FedAvg:
                 sys.exit(socket.SO_ERROR)
 
         print(f"Server listening on 0.0.0.0:{self.port}")
-        while True and self.client_index < self.n_clients:
+        while True and self.client_index <= self.n_clients:
+            print("Servicing client ", self.client_index)
             file_name = f'weights_{self.client_index}.pth'
             conn, addr = self.receiving_socket.accept()
             print(f"Connected by {addr}")
@@ -122,7 +123,6 @@ class FedAvg:
                         print("Client not available, waiting...")
                         time.sleep(15)
             self.receive_client_weights()
-
             for checkpoint in os.listdir(self.output_dir):
                 client_model = get_model(self.num_classes)
                 checkpoint = torch.load(join(self.output_dir, checkpoint), map_location=self.device)
@@ -146,6 +146,8 @@ class FedAvg:
                     shuffle=True,
                     wandb_logging=False,
                     wandb_project_name=None)
+                mean_aps.append(mean_ap)
+                mean_ars.append(mean_ar)
             self.client_index = 0
             step += 1
             if step > self.steps: